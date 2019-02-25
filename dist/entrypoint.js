b3b26b79cbcda3271eda0cc476d3e4ea812e48fb
mschilde <marko.schilde@gmail.com>
2019-02-25 21:44:13 +0100

    basic workflow

--- dist/entrypoint.js
+++ dist/entrypoint.js
@@ -1,4 +1,43 @@
 "use strict";
-const { Toolkit } = require('actions-toolkit');
-const tools = new Toolkit();
-console.log(tools.arguments);
+Object.defineProperty(exports, "__esModule", { value: true });
+const actions_toolkit_1 = require("actions-toolkit");
+const tools = new actions_toolkit_1.Toolkit({
+    event: ['pull_request.opened', 'pull_request.synchronize'],
+});
+exports.getPullRequests = (tools, { owner, repo, }) => {
+    const query = `{
+    repository(owner: "${owner}", name: "${repo}") {
+      pullRequests(last: 50, states:OPEN) {
+        edges {
+          node {
+            id
+            number
+            mergeable
+          }
+        }
+      }
+      labels(first: 100) {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  }`;
+    return tools.github.graphql(query, {
+        headers: { Accept: 'application/vnd.github.ocelot-preview+json' },
+    });
+};
+(async () => {
+    let result;
+    try {
+        result = await exports.getPullRequests(tools, tools.context.repo());
+    }
+    catch (error) {
+        console.error('Request failed: ', error.request, error.message);
+        tools.exit.failure('getPullRequests has failed.');
+    }
+    console.log('Result: ', result);
+})();
--- entrypoint.ts
+++ entrypoint.ts
@@ -1,4 +1,55 @@
-const { Toolkit } = require('actions-toolkit');
-const tools = new Toolkit();
+import { Toolkit } from 'actions-toolkit';
 
-console.log(tools.arguments);
+const tools = new Toolkit({
+  event: ['pull_request.opened', 'pull_request.synchronize'],
+});
+
+export const getPullRequests = (
+  tools: Toolkit,
+  {
+    owner,
+    repo,
+  }: {
+    owner: string;
+    repo: string;
+  },
+) => {
+  const query = `{
+    repository(owner: "${owner}", name: "${repo}") {
+      pullRequests(last: 50, states:OPEN) {
+        edges {
+          node {
+            id
+            number
+            mergeable
+          }
+        }
+      }
+      labels(first: 100) {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  }`;
+
+  return tools.github.graphql(query, {
+    headers: { Accept: 'application/vnd.github.ocelot-preview+json' },
+  });
+};
+
+(async () => {
+  let result;
+
+  try {
+    result = await getPullRequests(tools, tools.context.repo());
+  } catch (error) {
+    console.error('Request failed: ', error.request, error.message);
+    tools.exit.failure('getPullRequests has failed.');
+  }
+
+  console.log('Result: ', result);
+})();
