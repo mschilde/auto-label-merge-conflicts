bb7fea0d0e674399466cde554db8f6c31fdd9791
Marko Schilde <marko.schilde@gmail.com>
2020-01-28 11:49:43 +0100

    hello world test

---
+++ __tests__/index.ts
@@ -0,0 +1,7 @@
+describe('Auto label merge conflicts', () => {
+  describe('Parameters', () => {
+    test('should require CONFLICT_LABEL_NAME', () => {
+      expect(1).toBe(1);
+    });
+  })
+});
